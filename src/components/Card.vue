296eb05f15ad20cba2d828cd0fe17b94a19fa23b
kim kyeong min <kyeongmin21@naver.com>
2022-06-20 22:06:57 +0900

    feat : card 컴포넌트로 로딩데이터 불러오기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>Card</h1>
-    <div>card Id : {{ cardId }}</div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>
+      <div>card Id : {{ cardId }}</div>
+    </div>
+
   </div>
 </template>
 
@@ -10,16 +14,26 @@ export default {
   name: 'Card',
   data() {
     return {
-      cardId: 0
+      cardId: 0,
+      loading: false
     }
   },
   watch: {
     '$route'() {
-      this.cardId = this.$route.params.cardId
+      this.fetchData()
     }
   },
+  created() {
+    this.fetchData()
+  },
   methods: {
-
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cardId = this.$route.params.cardId
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
