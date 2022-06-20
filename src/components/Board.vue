a5bfef67c2a792514bc8df429af020bcfd8ed592
kim kyeong min <kyeongmin21@naver.com>
2022-06-20 21:49:21 +0900

    feat : v-if 문으로 로딩만들어 주는데 setTimeout 으로 비동기처리

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>Board</h1>
-    <p>board Id : {{ boardId }}</p>
-    <router-link :to="`/board/${boardId}/card/1`">card 1</router-link> |
-    <router-link :to="`/board/${boardId}/card/2`">card 2</router-link>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <p>board Id : {{ boardId }}</p>
+      <router-link :to="`/board/${boardId}/card/1`">card 1</router-link> |
+      <router-link :to="`/board/${boardId}/card/2`">card 2</router-link>
+    </div>
     <hr>
     <router-view></router-view>
   </div>
@@ -14,11 +17,21 @@ export default {
   name: 'Board',
   data() {
     return {
-      boardId: 0
+      boardId: 0,
+      loading: false
     }
   },
   created() {
-    this.boardId = this.$route.params.id
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.boardId = this.$route.params.id
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
