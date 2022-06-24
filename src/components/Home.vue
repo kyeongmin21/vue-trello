beffc08543ce0230f81c9d82a0a6bd381ce3edda
kim kyeong min <kyeongmin21@naver.com>
2022-06-24 21:35:38 +0900

    api 만들기

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,8 @@
       <div v-if="loading">Loading...</div>
       <div v-else>
         <div v-for="item in boards" :key="item.id">{{ item }}</div>
-        Api result: {{ boards }}</div>
+        Api result: {{ boards }}
+      </div>
       <ul>
         <li>
           <router-link to="/board/1">board 1</router-link>
@@ -20,11 +21,10 @@
 </template>
 
 <script>
-import axios from 'axios'
+import { board } from '../api'
 
 export default {
   name: 'Home',
-  components: {},
   data() {
     return {
       loading: false,
@@ -37,28 +37,39 @@ export default {
   methods: {
     fetchData() {
       this.loading = true
-      axios.get('http://localhost:3000/boards')
-        .then(res => {
-          this.boards = res.data
-        })
-        .catch(() => {
-          this.$router.replace('/login')
+      board.fetch()
+        .then(data => {
+          this.boards = data
         })
         .finally(() => {
           this.loading = false
         })
 
-/*      const req = new XMLHttpRequest()
-      req.open('GET', 'http://localhost:3000/health')
-      req.send() // client > server 요청이 날라감
-      req.addEventListener('load', () => {
-        this.loading = false
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      })
+      /*
+        axios.get('http://localhost:3000/boards')
+          .then(res => {
+            this.boards = res.data
+          })
+          .catch(() => {
+            this.$router.replace('/login')
+          })
+          .finally(() => {
+            this.loading = false
+          })
+      */
+
+      /*
+        const req = new XMLHttpRequest()
+        req.open('GET', 'http://localhost:3000/health')
+        req.send() // client > server 요청이 날라감
+        req.addEventListener('load', () => {
+          this.loading = false
+          this.apiRes = {
+            status: req.status,
+            statusText: req.statusText,
+            response: JSON.parse(req.response)
+          }
+        })
       */
     }
   }
