87dc5dc281cfc0a855100c36505f8ad1203f92fb
kim kyeong min <kyeongmin21@naver.com>
2022-07-02 13:36:34 +0900

    addBoard 컴포넌트 추가, 모달창 연결하기

---
+++ src/components/AddBoard.vue
@@ -0,0 +1,61 @@
+<template>
+  <modal>
+  <div slot="header">
+    <h2>
+      Create new board
+      <a href="" class="modal-default-button"
+         @click.prevent="close">&times;</a>
+    </h2>
+  </div>
+  <div slot="body">
+    <form id="add-board-form"
+          @submit.prevent="addBoard">
+      <input class="form-control" type="text" v-model="input" ref="input">
+    </form>
+  </div>
+  <div slot="footer">
+    <button class="btn"
+            type="submit"
+            :class="{ 'btn-success': valid }"
+            form="add-board-form"
+            :disabled="!valid">
+            Create Board</button>
+  </div>
+  </modal>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  name: "AddBoard",
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      input: '',
+      valid: false
+    }
+  },
+  watch: {
+    input(value) {
+      this.valid = value.trim().length > 0
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    },
+    addBoard () {
+      this.$emit('close')
+      this.$emit('submit', this.input)
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
