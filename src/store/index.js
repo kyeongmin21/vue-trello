0ae181d495fdf76e190917b902dbc9ad4f016ffa
kim kyeong min <kyeongmin21@naver.com>
2022-07-07 20:42:01 +0900

    로그인 : action은 비동기 로직 담당

--- src/store/index.js
+++ src/store/index.js
@@ -1,52 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { board, setAuthInHeader } from '../api'
+import { auth, board, setAuthInHeader} from '../api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    isAddBoard: false,
-    boards: [],
-    token: null
-  },
-  getters: {
-    isAuth (state) {
-      return !!state.token
-    }
-  },
-  mutations: {
-    SET_IS_ADD_BOARD (state, toggle) {
-      state.isAddBoard = toggle
+    state: {
+        isAddBoard: false,
+        boards: [],
+        token: null
     },
-    SET_BOARDS (state, boards) {
-      state.boards = boards
+    getters: {
+        isAuth(state) {
+            return !!state.token
+        }
     },
-    LOGIN (state, token) {
-      if (!token) return
-      state.token = token
-      localStorage.setItem('token', token)
-      setAuthInHeader(token)
+    mutations: {
+        SET_IS_ADD_BOARD(state, toggle) {
+            state.isAddBoard = toggle
+        },
+        SET_BOARDS(state, boards) {
+            state.boards = boards
+        },
+        LOGIN(state, token) {
+            if (!token) return
+            state.token = token
+            localStorage.setItem('token', token)
+            setAuthInHeader(token)
+        },
+        LOGOUT(state) {
+            state.token = null
+            delete localStorage.token
+            setAuthInHeader(null)
+        }
     },
-    LOGOUT (state) {
-      state.token = null
-      delete localStorage.token
-      setAuthInHeader(null)
-    }
-  },
-  actions: {
-    ADD_BOARD (_, { title }) {
-      return board.create(title)
+    actions: {
+        ADD_BOARD(_, {title}) {
+            return board.create(title)
+        },
+        FETCH_BOARDS({commit}) {
+            return board.fetch()
+                .then(data => {
+                    commit('SET_BOARDS', data.list)
+                })
+        },
+      LOGIN( {commit}, {email, password}) {
+          return auth.login(email, password)
+              .then(({accessToken}) => commit('LOGIN', accessToken))
+      }
     },
-    FETCH_BOARDS ({ commit }) {
-      return board.fetch()
-          .then(data => {
-            commit('SET_BOARDS', data.list)
-          })
-    }
-  },
-  modules: {
-  }
+    modules: {}
 })
 
 
