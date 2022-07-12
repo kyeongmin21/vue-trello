8a23637e9dfa71968104a5023a64f94afb299b9c
kim kyeong min <kyeongmin21@naver.com>
2022-07-12 21:32:23 +0900

    AddCard 컴포넌트 생성

---
+++ src/components/AddCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="add-card">
+    <input type="text" class="form-control">
+    <button type="submit" class="btn btn-success">Add Card</button>
+    <a href="" class="cancel-add-btn">&times;</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddCard",
+  data() {
+    return {
+      inputTitle: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.add-card {
+  padding: 10px;
+  display: block;
+  position: relative;
+}
+.add-card .cancel-add-btn {
+  display: inline-block;
+  margin-left: 10px;
+  vertical-align: middle;
+  text-decoration: none;
+  color: #888;
+  font-size: 24px;
+}
+.add-card .cancel-add-btn:hover,
+.add-card .cancel-add-btn:focus {
+  color: #666;
+}
+</style>
