980516f82d582b6e370d32734cd283004eb15b7f
kim kyeong min <kyeongmin21@naver.com>
2022-07-16 17:55:08 +0900

    카드 상세 조회 API 연동

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,49 +1,69 @@
 <template>
-  <div>
-    <Modal>
-      <h1>Card</h1>
-      <div v-if="loading">loading card...</div>
-      <div v-else>
-        <div>card Id : {{ cardId }}</div>
+  <modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" readonly>
       </div>
-    </Modal>
-  </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
+    </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea  class="form-control"
+                 cols="30"
+                 rows="3"
+                 placeholder="Add a more detailed description..."
+                 readonly
+                 v-model="card.description"></textarea>
+    </div>
+    <div slot="footer"></div>
+  </modal>
 </template>
 
 <script>
 import Modal from './Modal'
+import {mapState, mapActions} from 'vuex'
 
 export default {
   name: 'Card',
   components: {
     Modal
   },
-  data() {
-    return {
-      cardId: 0,
-      loading: false
-    }
-  },
-  watch: {
-    '$route'() {
-      this.fetchData()
-    }
+  computed: {
+    ...mapState({
+      card: 'card',
+      board: 'board'
+  })
   },
   created() {
-    this.fetchData()
+    const id = this.$route.params.cardId
+    this.FETCH_CARD({id})
   },
   methods: {
-    fetchData() {
-      this.loading = true
-      setTimeout(() => {
-        this.cardId = this.$route.params.cardId
-        this.loading = false
-      }, 500)
+    ...mapActions(['FETCH_CARD']),
+    onClose() {
+      this.$router.push(`/board/${this.board.id}`)
     }
   }
 }
 </script>
 
 <style>
-
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
 </style>
