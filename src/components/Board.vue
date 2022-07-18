2630b13edfa8ea7cf5fad764391941ecfcafa21e
kim kyeong min <kyeongmin21@naver.com>
2022-07-18 21:17:19 +0900

    card에 대한 position 값 설정

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import {mapState, mapActions} from 'vuex'
 import List from './List'
 import dragula from 'dragula'
 import 'dragula/dist/dragula.css'
@@ -51,14 +51,47 @@ export default {
     this.dragularCards = dragula([
       // 유사배열로 만들어줌
       ...Array.from(this.$el.querySelectorAll('.card-list'))
-    ])
+    ]).on('drop', (el, wrapper, target, siblings) => {
+      // 어디를 이동해야할지 정보를 담는 카드
+      const targetCard = {
+        id: el.dataset.cardId * 1,
+        pos: 65535
+      }
+
+      let prevCard = null
+      let nextCard = null
+
+      Array.from(wrapper.querySelectorAll('.card-item'))
+        .forEach((el, idx, arr) => {
+          const cardId = el.dataset.cardId * 1
+          if (cardId === targetCard.id) {
+            prevCard = idx > 0 ? {
+              id: arr[idx - 1].dataset.cardId * 1,
+              pos: arr[idx - 1].dataset.cardPos * 1,
+            } : null
+            nextCard = idx < arr.length - 1 ? {
+              id: arr[idx + 1].dataset.cardId * 1,
+              pos: arr[idx + 1].dataset.cardPos * 1
+            } : null
+          }
+        })
+
+      // 앞에 없고 뒤에만 있으니깐 : 맨앞에 있다는 뜻
+      if (!prevCard && nextCard) targetCard.pos = nextCard.pos / 2
+      // 맨뒤에 있는 카드
+      else if (prevCard && !nextCard) targetCard.pos = prevCard.pos * 2
+      // 중간에 있는 카드
+      else if (prevCard && nextCard) targetCard.pos = (prevCard.pos + nextCard.pos) / 2
+    })
+
+
   },
   methods: {
     ...mapActions(['FETCH_BOARD']),
     fetchData() {
       this.loading = true
-      this.FETCH_BOARD({ id: this.$route.params.id })
-      .then(() => this.loading = false)
+      this.FETCH_BOARD({id: this.$route.params.id})
+        .then(() => this.loading = false)
     }
   }
 }
@@ -72,11 +105,13 @@ export default {
   right: 0;
   left: 0;
 }
+
 .board {
   display: flex;
   flex-direction: column;
   height: 100%;
 }
+
 .board-header {
   flex: none;
   padding: 8px 4px 8px 8px;
@@ -84,9 +119,11 @@ export default {
   height: 32px;
   line-height: 32px;
 }
+
 .board-header input[type=text] {
   width: 200px;
 }
+
 .board-header-btn {
   border-radius: 4px;
   padding: 2px 10px;
@@ -95,24 +132,29 @@ export default {
   display: inline-block;
   color: #fff;
 }
+
 .board-header-btn:hover,
 .board-header-btn:focus {
-  background-color: rgba(0,0,0,.15);
+  background-color: rgba(0, 0, 0, .15);
   cursor: pointer;
 }
+
 .board-title {
   font-weight: 700;
   font-size: 18px;
 }
+
 .show-menu {
   font-size: 14px;
   position: absolute;
   right: 15px;
 }
+
 .list-section-wrapper {
   flex-grow: 1;
   position: relative;
 }
+
 .list-section {
   position: absolute;
   top: 0;
@@ -124,6 +166,7 @@ export default {
   white-space: nowrap;
   padding: 0 10px;
 }
+
 .list-wrapper {
   display: inline-block;
   height: 100%;
@@ -131,9 +174,11 @@ export default {
   vertical-align: top;
   margin-right: 5px;
 }
+
 .card-item.gu-transit {
   background-color: #555 !important;
 }
+
 .card-item.gu-mirror {
   opacity: 1 !important;
   background-color: #fff !important;
