b07a257de72dbe540ab6d9b405d8273edfdfbaeb
kim kyeong min <kyeongmin21@naver.com>
2022-07-26 21:20:31 +0900

    AddList 컴포넌트 생성

---
+++ src/components/AddList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="add-list">
+    <input type="text" class="form-control">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddList"
+}
+</script>
+
+<style scoped>
+
+</style>
