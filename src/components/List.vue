89935901071c25772af8ab831f76055099820ef3
kim kyeong min <kyeongmin21@naver.com>
2022-07-27 21:07:37 +0900

    inputTitle 이 클릭하면 focus되게하고, 수정가능하게 구현 / v-model로 원래 써져있는 글씨도 나오게 하기

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="list">
     <div class="list-header">
-      <div class="list-header-title">{{ data.title }}</div>
+      <input type="text"
+             v-if="isEditTitle"
+             class="form-control input-title"
+             ref="inputTitle"
+      v-model="inputTitle">
+      <div v-else
+           class="list-header-title"
+           @click="onClickTitle">
+        {{ data.title }}
+      </div>
     </div>
 
     <div class="card-list">
@@ -9,13 +18,14 @@
     </div>
 
     <div v-if="isAddCard">
-      <AddCard :list-id="data.id" @close="isAddCard = false"></AddCard>
+      <AddCard :list-id="data.id"
+               @close="isAddCard = false"></AddCard>
     </div>
-   <div v-else>
-     <a href="" class="add-card-btn" @click.prevent.stop="isAddCard = true">
-       &plus; Add a card..
-     </a>
-   </div>
+    <div v-else>
+      <a href="" class="add-card-btn" @click.prevent.stop="isAddCard = true">
+        &plus; Add a card..
+      </a>
+    </div>
 
   </div>
 </template>
@@ -31,9 +41,22 @@ export default {
     CardItem
   },
   props: ['data'],
+  created() {
+    // title 클릭했을 때도 todo라는 글씨가 나오게 하기
+    this.inputTitle = this.data.title
+  },
   data() {
     return {
-      isAddCard: false
+      isAddCard: false,
+      isEditTitle: false,
+      inputTitle: ''
+    }
+  },
+  methods: {
+    onClickTitle() {
+      this.isEditTitle = true
+      // 다음 렌더링사이클에서 돌아가도록 nextTick 감싸준다.
+      this.$nextTick(() => this.$refs.inputTitle.focus())
     }
   }
 }
@@ -50,21 +73,25 @@ export default {
   width: 100%;
   max-height: 100%;
 }
+
 .list-header {
   flex: 0 0 auto;
   height: 30px;
   padding: 10px 8px 8px;
   position: relative;
 }
+
 .list-header-title {
   font-size: 16px;
   font-weight: 700;
   padding-left: 8px;
   line-height: 30px;
 }
+
 .input-title {
   width: 90%;
 }
+
 .delete-list-btn {
   position: absolute;
   right: 10px;
@@ -73,15 +100,18 @@ export default {
   color: #aaa;
   font-size: 24px;
 }
+
 .card-list {
   flex: 1 1 auto;
   overflow-y: scroll;
 }
-.empty-card-item   {
+
+.empty-card-item {
   height: 10px;
   width: 100%;
-  background-color: rgba(0,0,0, 0);
+  background-color: rgba(0, 0, 0, 0);
 }
+
 .add-card-btn {
   flex: 0 0 auto;
   display: block;
@@ -89,8 +119,9 @@ export default {
   color: #8c8c8c;
   text-decoration: none;
 }
+
 .add-card-btn:focus,
 .add-card-btn:hover {
-  background-color: rgba(0,0,0, .1);
+  background-color: rgba(0, 0, 0, .1);
 }
 </style>
